2b56385b423c89cb5be793a85fa6c918e3e01e37
parita <trstiphone@gmail.com>
2024-12-24 10:24:29 -0800

    first commit

---
+++ database/migrations/2024_12_03_165749_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Hamcrest\Description;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->mediumText('description');
+            $table->string('image');
+            $table->tinyInteger('status')->default('0');
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
